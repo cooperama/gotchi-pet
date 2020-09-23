d2b23ca98fcf80079ed414c6a24df9b1e98a6b48
cooperama <coope133@gmail.com>
2020-09-23 15:18:33 -0700

    Commit 10 - Body and screen backgrounds and animations added

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,13 @@
 // ----------------------------- Tamagotchi
 
 // - Global Variables
-// - Functions
 // - Tamagotchi Class
 // - Game Object
 // - Events
 
 // ----------------------------- Global Variables
 
+const startButton = document.querySelector('.start-game');
 const feedButton = document.getElementById('feed');
 const playButton = document.getElementById('play');
 const teachButton = document.getElementById('teach');
@@ -17,46 +17,24 @@ const hungryBar = document.getElementById('hungry-progress');
 const sleepyBar = document.getElementById('sleepy-progress');
 const boredBar = document.getElementById('bored-progress');
 const skillsBar = document.getElementById('skills-progress');
-// const statBarList = [hungryBar, sleepyBar, boredBar];
-// const statsUl = document.querySelector('.stats');
 const allBars = document.querySelectorAll('.bar')
 
 const ageSpan = document.querySelector('.age');
 const nameSpan = document.querySelector('.name')
 
 const messageP = document.querySelector('.message');
+const messageBoxDiv = document.querySelector('.message-box');
+const statsBoxDiv = document.querySelector('.stats-box');
+const interactionsDiv = document.querySelector('.interactions');
 
-let time = 0;
-let gameplay = 'off';
+const moonImg = document.querySelector('.moon');
 
-// ----------------------------- Functions
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const screen = document.querySelector('.screen');
 
-// start timer of pet counting up, increment sleep, boredome, and hunger with time
-function lifetimer(tamagotchi) {
-  const timerId = setInterval(function() {
-    console.log(time);
-    if (time % 15 === 0) {
-      const sleepyVal = tamagotchi.stats.sleepy++;
-      console.log('sleepyVal: ', sleepyVal)
-      sleepyBar.style.width = `${sleepyVal*10}%`;
-    };
-    if (time % 3 === 0) {
-      const boredVal = tamagotchi.stats.bored++;
-      const hungryVal = tamagotchi.stats.hungry++;
-      console.log('boredVal: ', boredVal)
-      console.log('hungry Val: ', hungryVal)
-      boredBar.style.width = `${boredVal*10}%`;
-      hungryBar.style.width = `${hungryVal*10}%`;
-    };
-    time++;
-    // If any stats reach 10, game over.
-    tamagotchi.checkStats();
-    if (gameplay === 'off') {
-      clearInterval(timerId);
-    }
-  }, 1000);
-
-}
 
 // ----------------------------- Tamagotchi Class
 
@@ -120,6 +98,8 @@ class Gotchi {
     this.age++;
     ageSpan.textContent = this.age;
     this.checkStats();
+    // change color
+    this.changeColor();
   }
   eat() {
     this.stats.hungry = 0;
@@ -130,7 +110,7 @@ class Gotchi {
     this.stats.bored <= 2 ? this.stats.bored = 0 : this.stats.bored -= 2;
     this.stats.sleepy += 0.5;
     this.stats.skill += 0.25;
-    this.stats.hungry += 0.5;
+    this.stats.hungry += 1.5;
     boredBar.style.width = `${this.stats.bored*10}%`;
     sleepyBar.style.width = `${this.stats.sleepy*10}%`;
     this.checkStats();
@@ -139,50 +119,26 @@ class Gotchi {
     this.stats.bored <= 2 ? this.stats.bored = 0 : this.stats.bored -= 2;
     this.stats.sleepy += 0.5;
     this.stats.skill++;
-    this.stats.hungry++;
+    this.stats.hungry += 1.5;
     boredBar.style.width = `${this.stats.bored*10}%`;
     sleepyBar.style.width = `${this.stats.sleepy*10}%`;
-    console.log('this.stats.bored after: ', `${this.stats.bored*10}%`)
-    console.log('this.stats.sleepy after: ', `${this.stats.sleepy*10}%`)
     skillsBar.style.width = `${this.stats.skill*10}%`;
   }
   // Change color based on food eaten
   changeColor() {
-    console.log('this.color before: ', this.color)
-    const foods = this.care.food.keys();
+    const foods = Object.keys(this.care.food);
     let mostEaten;
     let amountEaten = 0;
     for (let i = 0; i < foods.length; i++) {
-      if (this.care[foods[i]] > amountEaten) {
+      if (this.care.food[foods[i]] > amountEaten) {
         mostEaten = foods[i];
         amountEaten = this.care[foods[i]];
       }
     }
     this.color = this.character.temper[mostEaten][1];
-    console.log('this.color after: ', this.color)
   }
   // activate animations when stats are in danger zone
   checkStats() {
-    // let statNums = Object.values(this.stats);
-    // let danger = statNums.slice(0, statNums.length - 1).some(num => {
-    //   if (num >= 10) {
-    //     messageP.textContent = `${this.name} isn't responding anymore...`;
-    //     game.isAlive = false;
-    //     gameplay = 'off';       // stops lifetimer
-    //   } else if (num >= 8) {
-    //     messageP.textContent = `${this.name} doesn't feel so good...`;
-    //   } 
-    // });
-    // console.log(danger)
-    // // change color of progress bars that are at 8 or above;
-    // if (danger) {
-    //   let statBars = Object.values(this.stats);
-    //   statBars.foreach(statbar => {
-    //     console.log(statbar)
-    //     if (this.stats[statbar] >= 8) {
-    //     }
-    //   })
-    // }
     let statNums = Object.values(this.stats);
     statNums = statNums.slice(0, statNums.length - 1);
     // Change message if stats go above 8
@@ -193,44 +149,34 @@ class Gotchi {
       } 
     });
     // Check if any stats are at 10, if so, game over
-    statNums.some( num => {
+    let dead = statNums.some( num => {
       if (num > 10) {
         messageP.textContent = `${this.name} isn't responding anymore...`;
         game.isAlive = false;
-        gameplay = 'off';       // stops lifetimer
+        // gameplay = 'off';       // stops life()
+        return true;
       } 
     })
-    console.log(danger)
     // change color of progress bars that are at 8 or above;
     // trickyyyyyyyy couldn't select just one bar
     if (danger) {
       allBars.forEach(bar => {
         bar.classList.add('warning');
       })
-      // let statBars = Object.keys(this.stats);
-      // statBars.forEach(statbar => {
-      //   if (this.stats[statbar] >= 8) {
-      //     console.log(statbar)
-      //     // change color of progress bar
-      //   }
-      // })
-      // let statBars = Object.keys(this.stats);
-      // statBarList.forEach(statbar => {
-      //   if () {
-      //     console.log(statbar)
-      //     statbar.style.backgroundColor = 'orange';
-      //     // change color of progress bar
-      //   }
-      // })
     } else {
       allBars.forEach(bar => {
         bar.classList.remove('warning');
       })
     }
+    // commented out below so game can continue during testing ////
+    // if (dead) {
+    //   messageBoxDiv.remove();
+    //   statsBoxDiv.remove();
+    //   interactionsDiv.remove();
+    //   // move name to center, put Gotchi in a grave //////////////////
+    // }
 
   }
-
-
   // Update desire/skill if age is 6 activate animations to hint at Tamagotchi's desire
   checkAge() {
     if (this.age === 6) {
@@ -242,6 +188,8 @@ class Gotchi {
     }
     if (this.age === 10) {
       // activate wants a change scene
+      this.result = this.getDesire();
+      // return this.choices[this.result];
     }
   }
   getDesire() {
@@ -276,34 +224,123 @@ class Gotchi {
 // ----------------------------- Game Object
 
 const game = {
-  
+  gotchis: [],
+  isAlive: false,
+  time: 0,
+  intro() {
+    // gotchi hatches, show statsBoxDiv, interactionsDiv, messageDiv,
+    // DONT show name ~~ that comes after intro
+    // explain game rules
+  },
+  start() {
+    this.isAlive = true;
+    this.intro();
+    const gotchiName = prompt('What will you call your Tamagotchi?');
+    const tamagotchi = new Gotchi(gotchiName);
+    // update name~~
+    nameSpan.textContent = gotchiName;
+    // in case I want to develop so more gotchis are born::::
+    this.gotchis.push(tamagotchi);
+
+    // instead of prompt... a form might be better...
+    // const gotchiName = prompt('What will you call your Tamagotchi?');
+    // const tamagotchi = new Gotchi(gotchiName);
+    header.classList.remove('display-none');
+    main.classList.remove('display-none');
+    footer.classList.remove('display-none');
+    // life(this.gotchis[0]);
+    this.life(this.gotchis[0]);
+  },
+  life(tamagotchi) {
+    const timerId = setInterval(function() {
+      console.log(game.time);
+      if (game.time % 15 === 0) {
+        const sleepyVal = tamagotchi.stats.sleepy++;
+        sleepyBar.style.width = `${sleepyVal*10}%`;
+      };
+      if (game.time % 3 === 0) {
+        const boredVal = tamagotchi.stats.bored++;
+        const hungryVal = tamagotchi.stats.hungry++;
+        boredBar.style.width = `${boredVal*10}%`;
+        hungryBar.style.width = `${hungryVal*10}%`;
+      };
+      game.time++;
+      // If any stats reach 10, game over.
+      tamagotchi.checkStats();
+      // if (this.gameplay === 'off') {
+      if (this.isAlive === false) {
+        clearInterval(timerId);
+        this.gameOver();
+      }
+    }, 1000);
+  },
+  gameOver() {
+    // if gotchi dies
+  }
 }
 
 
 // ----------------------------- Events
 
 
+startButton.addEventListener('click', function() {
+  // remove start button,
+  startButton.remove();
+  // start float down animation
+  moonImg.classList.add('float-down');
+  moonImg.classList.add('wobble');
+  // when animation is done, 
+  // change background images of body and screen (maybe fade the screen to white for day)
+  let waiting = true;
+  let wait = 0;
+  while (waiting) {
+    const pause = setInterval(function() {
+      if (wait === 6) {
+        body.style.background = 'url(./images/ghilbli_day_2.jpg)';
+        screen.style.background = 'url(./images/ghibli_background.jpg)';
+        // moonImg.remove();
+        clearInterval(pause);
+        waiting = false;
+      }
+      wait++
+    }, 1000);
+    break;
+  }
+
+
+
+  // remove moon image
+  // show gotchi image
+  // show message, stats, and interaction boxes
+})
+
+// maybe have the user click the tamagotchi before clicking a button that way, the captured element can be passed into the event listeners below
+// buttons are inactive until tamagotchi is clicked, then they are active and can be clicked.
+
 feedButton.addEventListener('click', function(e) {
-  gotchi.eat();
+  game.gotchis[0].eat();
 });
 playButton.addEventListener('click', function(e) {
-  gotchi.play();
+  game.gotchis[0].play();
 });
 teachButton.addEventListener('click', function(e) {
-  gotchi.learn();
+  game.gotchis[0].learn();
 });
 tuckButton.addEventListener('click', function(e) {
-  gotchi.sleep();
+  game.gotchis[0].sleep();
 });
 
 
-// testing testing testing
+// ----------------------------- testing testing testing
 
-const gotchi = new Gotchi('Gotchi');
-gameplay = 'on';
-lifetimer(gotchi);
+// const gotchi = new Gotchi('Gotchi');
+// gameplay = 'on';
+// game.life(gotchi);
 // gotchi.checkStats();
 // gotchi.getDesire();
 // gotchi.getSkills();
+// gotchi.checkAge();
+
 
 // testing testing testing
+// game.start();
